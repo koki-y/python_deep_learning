fc4431c44aa49f51dc38fae112e5921bbd747372
koki-y <kokiydp@gmail.com>
2020-01-26 18:53:11 +0900

    Add pydoc

--- model/layer/affine.py
+++ model/layer/affine.py
@@ -1,9 +1,39 @@
 import numpy as np
 
 class Affine:
+    """
+    Affine calculation layer.
+
+    Attributes
+    ---------------
+    params : list
+        The model parameters. (weight, bias)
+    grads : list
+        The learning gradients. (weight, bias)
+    x : list
+        The input of affine calculation.
+    """
+
     def __init__(self, W, b):
         self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
 
     def forward(self, x):
+        """
+        Return the result of affine calculation.
+
+        Parameters
+        ----------
+        x : vector (or matrix)
+            The input of affine calculation.
+
+        Returns
+        -------
+        out : vector (or matrix)
+            The output of affine calculation.
+        """
         W, b = self.params
-        return np.dot(x, W) + b
+        out = np.dot(x, W) + b
+        self.x = x
+        return out
--- model/layer/sigmoid.py
+++ model/layer/sigmoid.py
@@ -1,9 +1,56 @@
 import numpy as np
 
 class Sigmoid:
+    """
+    Sigmoid function layer.
+
+    Attributes
+    ---------------
+    params : list
+        The model parameters. (weight, bias)
+        There's no elements for this layer.
+    grads : list
+        The learning gradients. (weight, bias)
+        There's no elements for this layer.
+    out : double
+        The output of forwarding.
+    """
+
     def __init__(self):
         self.params = []
+        self.grads = []
+        self.out = None
 
     def forward(self, x):
-        return 1 / (1 + np.exp(-x))
+        """
+        Return the result of sigmoid function.
+        If the input is vector, it returns vector as result.
+
+        Parameters
+        ----------
+        x : Real number (or vector)
+            The input of sigmoid function.
+
+        Returns
+        -------
+        out : Real number (or vector)
+            The output of sigmoid function.
+        """
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, d_out):
+        """
+        Parameters
+        ----------
+        d_out : double
+            The backpropagation value from next layer.
+
+        Returns
+        -------
+        out : double
+            The backpropagation value for previous layer.
+        """
+        return d_out * (1.0 - self.out) * self.out
 
