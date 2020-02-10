90f01f21b978d2f90564049280de00e893b1785a
koki-y <koki-y@sra.co.jp>
2020-02-10 11:04:55 +0900

    Added sample script for spiral data.

---
+++ spiral_sample.py
@@ -0,0 +1,22 @@
+from dataset   import spiral
+from model     import TwoLayerNet
+from optimizer import SGD
+from trainer   import MiniBatchTrainer
+from common    import pickler
+
+hidden_size = 10
+learning_rate = 1.0
+
+# Load training data.
+x, t = spiral.load_data()
+
+# Create mini batch trainer with TwoLayerNet model and SGD optimizer.
+model = TwoLayerNet(x.shape[1], hidden_size, t.shape[1])
+optimizer = SGD(learning_rate)
+trainer = MiniBatchTrainer(model, optimizer)
+
+x_data_size = x.shape[0]
+trainer.train(x=x, t=t, epoch=300, batch_size=(x_data_size//10))
+
+# Save trained model
+pickler.save(model)
