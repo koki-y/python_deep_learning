fbed59491a2aca47cd5e1e08752a68fb6e6118b1
koki-y <koki-y@sra.co.jp>
2020-02-10 10:24:06 +0900

    Added spiral data set.

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 def show_image(img):
     Image.fromarray(np.uint8(img.reshape(28, 28))).show()
@@ -8,3 +9,10 @@ def show_answer(ans):
     ans = list(ans)
     print("The answer is " + str(ans.index(max(ans))) + ".")
 
+def show_spiral_data(x):
+    N = 100
+    CLS_NUM = 3
+    markers = ['o', 'x', '^']
+    for i in range(CLS_NUM):
+        plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
+    plt.show()
