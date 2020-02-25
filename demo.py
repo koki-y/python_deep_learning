0c52f48db0f1bb7feb6394f9b33e59e609ab6895
koki-y <kokiydp@gmail.com>
2020-02-25 21:53:35 +0900

    Added demo function.

--- demo.py
+++ demo.py
@@ -54,8 +54,7 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
         return
 
     print(f'Query: {query}')
-    query_id = word_to_id[query]
-    query_vec = word_matrix[query_id]
+    query_vec = word_matrix[word_to_id[query]]
 
     vocab_size = len(id_to_word)
 
@@ -72,4 +71,40 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
         count += 1
         if count >= top:
             return
+
+def analogy(a, b, c, word_to_id, id_to_word, word_matrix, top=5, answer=None):
+    for word in (a, b, c):
+        if word not in word_to_id:
+            print(f'{word} is not found.')
+            return
+
+    print(f'[analogy] {a} : {b} = {c} : ?')
+    a_vec = word_matrix[word_to_id[a]]
+    b_vec = word_matrix[word_to_id[b]]
+    c_vec = word_matrix[word_to_id[c]]
+
+    query_vec = normalize(b_vec - a_vec + c_vec)
+    similarity = np.dot(word_matrix, query_vec)
+
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if np.isnan(similarity[i]):
+            continue
+        if id_to_word[i] in (a, b, c):
+            continue
+        print(f'{id_to_word[i]} : {similarity[i]}')
+
+        count += 1
+        if count >= top:
+            return
+
+
+def normalize(x):
+    if x.ndim == 2:
+        s = np.sqrt((x * x).sum(1))
+        x /= s.reshape((s.shape[0], 1))
+    if x.ndim == 1:
+        s = np.sqrt((x * x).sum())
+        x /= s
+    return x
 
