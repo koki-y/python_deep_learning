Fixed grads calculation in LSTM.

model/layer/lstm.py
@@ -36,10 +36,17 @@ class LSTM:
         tanh_c_next = np.tanh(c_next)
         ds = dc_next + (dh_next * o) * (1 - tanh_c_next ** 2)
 
+        dc_prev = ds * f
+
         df = ds * c_prev
         di = ds * g
         do = dh_next * tanh_c_next
         dg = ds * i
+
+        df *= f * (1 - f)
+        di *= i * (1 - i)
+        do *= o * (1 - o)
+        dg *= (1 - g ** 2)
 
         dWh_f = np.dot(h_prev.T, df)
         dWx_f = np.dot(x.T, df)
@@ -70,7 +77,6 @@ class LSTM:
         dx = np.dot(df, Wx_f.T) + np.dot(di, Wx_i.T) + np.dot(do, Wx_o.T) + np.dot(dg, Wx.T)
         dh = np.dot(df, Wh_f.T) + np.dot(di, Wh_i.T) + np.dot(do, Wh_o.T) + np.dot(dg, Wh.T)
 
-        dc_prev = ds * i
         return dx, dh, dc_prev
 
 def sigmoid(x):

model/layer/time_lstm.py
@@ -48,16 +48,14 @@ class TimeLSTM:
 
         dxs = np.empty((N, T, D), dtype='f')
         dh, dc = 0, 0
-        grads = [0, 0, 0]
+        grads = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         for t in reversed(range(T)):
             layer = self.layers[t]
             dx, dh, dc = layer.backward(dhs[:, t, :] + dh, dc)
             dxs[:, t, :] = dx
 
-            dWx_f, dWh_f, db_f, dWx_i, dWh_i, db_i, dWx_o, dWh_o, db_o, dWx, dWh, db = layer.grads
-            grads[0] += dWx_f + dWx_i + dWx_o + dWx
-            grads[1] += dWh_f + dWh_i + dWh_o + dWh
-            grads[2] += db_f  + db_i  + db_o  + db
+            for i, grad in enumerate(layer.grads):
+                grads[i] += grad
 
         for i, grad in enumerate(grads):
             self.grads[i][...] = grad

rnnlm_sample.py
@@ -5,6 +5,31 @@ from trainer   import RnnlmTrainer
 from optimizer import SGD
 from common    import pickler
 
+def eval_perplexity(model, corpus, batch_size=10, time_size=35):
+    print('evaluating perplexity ...')
+    corpus_size = len(corpus)
+    total_loss, loss_cnt = 0, 0
+    max_iters = (corpus_size - 1) // (batch_size * time_size)
+    jump = (corpus_size - 1) // batch_size
+
+    for iters in range(max_iters):
+        xs = np.zeros((batch_size, time_size), dtype=np.int32)
+        ts = np.zeros((batch_size, time_size), dtype=np.int32)
+        time_offset = iters * time_size
+        offsets = [time_offset + (i * jump) for i in range(batch_size)]
+        for t in range(time_size):
+            for i, offset in enumerate(offsets):
+                xs[i, t] = corpus[(offset + t) % corpus_size]
+                ts[i, t] = corpus[(offset + t + 1) % corpus_size]
+        try:
+            loss = model.forward(xs, ts, train_flg=False)
+        except TypeError:
+            loss = model.forward(xs, ts)
+        total_loss += loss
+    print('')
+    ppl = np.exp(total_loss / max_iters)
+    return ppl
+
 batch_size   = 20
 wordvec_size = 100
 hidden_size  = 100
@@ -14,6 +39,7 @@ max_epoch    = 4
 max_grad     = 0.25
 
 corpus, word_to_id, id_to_word = ptb.load_data('train')
+corpus_test, _, _ = ptb.load_data('test')
 
 vocab_size = int(max(corpus) + 1)
 xs = corpus
@@ -24,7 +50,11 @@ optimizer = SGD(lr)
 trainer = RnnlmTrainer(model, optimizer)
 
 trainer.train(xs, ts, max_epoch, batch_size, time_size, max_grad)
-trainer.plot()
+# trainer.plot()
+
+model.reset_state()
+ppl_test = eval_perplexity(model, corpus_test)
+print(f'test perplexity:{ppl_test}')
 
 pickler.save(model, 'model_lstm')
 